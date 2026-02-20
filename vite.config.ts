7431a8a5480ffcf449db2a8002ff630f224317bf
zhigalovzxcvh-lgtm <zhigalov.zxcvh@gmail.com>
2026-02-21 00:25:47 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/coal-dashboard/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+});
